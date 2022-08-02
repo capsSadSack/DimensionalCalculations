68c8a374a2d7d0647db130e446636ba96e3ecece
capsSadSack <cherkashin@phystech.edu>
2022-08-02 14:55:12 +0300

    Projects renaming

---
+++ DimensionalCalculations/MathEquationParsing/MathEquationParsing.cs
@@ -0,0 +1,12 @@
+﻿using DimensionalCalculations;
+
+namespace StiringParsing
+{
+    public class MathEquationParsing
+    {
+        public PhysicalQuantity ParsePhysicalQuantity(string str)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
